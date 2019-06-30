f9d4bedbc4feb2c79d8184daf31adec8b209b589
Liron Lee <liron.li@outlook.com>
2019-06-30 17:20:47 +0800

    first commit

---
+++ lib/pages/mine_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MinePage extends StatefulWidget {
+  @override
+  _MinePage createState() => _MinePage();
+}
+
+class _MinePage extends State<MinePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('MinePage'),
+      ),
+    );
+  }
+}
---
+++ lib/pages/search_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPage createState() => _SearchPage();
+}
+
+class _SearchPage extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('SearchPage'),
+      ),
+    );
+  }
+}
